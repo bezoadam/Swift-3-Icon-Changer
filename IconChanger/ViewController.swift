a0d964cc2414e69118a79666e8720946c1d80bba
Adam Bezak <bezoadam95@gmail.com>
2017-04-19 18:03:06 +0200

    Initial Commit

---
+++ IconChanger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IconChanger
+//
+//  Created by Adam Bezák on 19.4.17.
+//  Copyright © 2017 Adam Bezák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
